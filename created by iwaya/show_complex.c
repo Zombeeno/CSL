edf97421bc92696581fc3f441d904a2ad6767312
Masaki1108 <56675993+Masaki1108@users.noreply.github.com>
2019-10-23 14:33:13 +0900

    Create show_complex.c

---
+++ created by iwaya/show_complex.c
@@ -0,0 +1,29 @@
+#include<math.h>
+#include<stdio.h>
+#include<complex.h>
+#include<stdlib.h>
+#include "cmatrix.h"
+/*複素数はdouble complex型で定義でき、虚数単位はIで入力*/
+
+void show_complex(double complex z){
+  printf("%f+%fi ",creal(z),cimag(z));
+}
+
+int main(void)
+{
+  double complex **A;
+  A = alloc_cmatrix(2,2);
+  for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+      mat_elem(A,i,j)=i*1.0+j*1.0*I;
+    }
+  }
+  printf("A=\n");
+  for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+    show_complex(mat_elem(A,i,j));}
+    printf("\n");
+  }
+  
+  return 0;
+}
